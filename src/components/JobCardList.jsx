7d9e156cea3548c4a663a774c7d393f144285585
pengpon <bd102pon@gmail.com>
2023-10-25 22:58:17 +0800

    fix: coding style

--- src/components/JobCardList.jsx
+++ src/components/JobCardList.jsx
@@ -1,15 +1,11 @@
-/* eslint-disable react/prop-types */
-import {
-  SimpleGrid,
-} from "@chakra-ui/react";
-
+import { SimpleGrid } from "@chakra-ui/react";
 import { JobCard } from "./JobCard";
 
-export default function JobCardList({jobs}) {
-  const jobsData = [...jobs]
-  const jobItems = jobsData.map((job) => {
-    return (<JobCard  key={job.key} job={job}></JobCard>)
-  })
+export default function JobCardList({ jobs }) {
+  const jobsData = [...jobs];
+  const jobItems = jobsData.map((job) => (
+    <JobCard key={job.key} job={job}></JobCard>
+  ));
 
   return (
     <SimpleGrid columns={{ md: 1, lg: 3 }} spacing={5}>
