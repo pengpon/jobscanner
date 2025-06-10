9a03bd06e3e8b142c7d29a9cacc93cc50a2ee46b
pengpon <bd102pon@gmail.com>
2025-06-10 16:14:01 +0800

    fix: check job item is null

--- src/utils/format.js
+++ src/utils/format.js
@@ -18,7 +18,7 @@ const jobFormat = (res) => {
   // filter duplicate key & replace location string
   const obj = {};
   data.forEach((item) => {
-    item.location = item.location.replaceAll('臺', '台')
+    if (item.location)  item.location = item.location.replaceAll('臺', '台')
     obj[`${item.key}`] = item;
   });
   return {result: Object.values(obj), updateTime: time};
